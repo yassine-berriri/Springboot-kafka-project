f9692ceab4a90a6d600f8b95a4fe88f5c367bd80
yassineBr12 <yassine.berriri@etu.univ-cotedazur.fr>
2024-10-15 17:34:59 +0200

    add kafka consumer

---
+++ src/main/java/net/javaguides/springboot_kafka_tutorial/kafka/JsonKafkaConsumer.java
@@ -0,0 +1,17 @@
+package net.javaguides.springboot_kafka_tutorial.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JsonKafkaConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JsonKafkaConsumer.class);
+    @KafkaListener(topics = "javaguides", groupId = "myGroup")
+    public void consume(String message) {
+        LOGGER.info(String.format("Message recieved -> %s", message));
+
+    }
+}
